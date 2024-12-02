2e90d6436de15c083194de6e52a99d7d4568be81
Kamida SYOU <e245709@eve.u-ryukyu.ac.jp>
2024-12-02 23:31:24 +0900

    third commit

---
+++ app/src/main/java/Main3.java
@@ -0,0 +1,8 @@
+import jp.ac.uryukyu.ie.e245709.*;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Hero hero = new Warrior("勇者", 10, 5);
+        System.out.println(hero);
+    }
+}
